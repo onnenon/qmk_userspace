36ab556d8ae9e1d9d1017dcda62f96aa0a864420
Stephen Onnen <stephen.onnen@gmail.com>
2024-11-26 17:36:04 -0600

    update

--- keyboards/sporkus/le_chiffre_32/keymaps/onnenon/keymap.c
+++ keyboards/sporkus/le_chiffre_32/keymaps/onnenon/keymap.c
@@ -58,35 +58,35 @@ const key_override_t *key_overrides[] = {&comma_key_override, &dot_key_override,
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT(
-    KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,           KC_MUTE            KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
+    KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,           KC_MUTE,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
     LGUI_T(KC_A),       LALT_T(KC_S),       LCTL_T(KC_D),       LSFT_T(KC_F),       KC_G,                              KC_H,           LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_QUOT),
     KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,                              KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,
                                                                 LT(NAV,KC_LSFT),    LT(NUM,KC_ENT),                    LT(SYM,KC_SPC), LT(FN,KC_BSPC)
 ),
 
 [NUM] = LAYOUT(
-    KC_1,              KC_2,              KC_3,              KC_4,              KC_5,               KC_MUTE            KC_6,              KC_7,              KC_8,              KC_9,              KC_0,
+    KC_1,              KC_2,              KC_3,              KC_4,              KC_5,               KC_MUTE,           KC_6,              KC_7,              KC_8,              KC_9,              KC_0,
     KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,                                  U_NA,              KC_4,              KC_5,              KC_6,              U_NA,
     U_NA,              U_NA,              U_NA,              U_NA,              U_NA,                                  KC_0,              KC_1,              KC_2,              KC_3,              U_NA,
                                                              U_NA,              U_NA,                                  U_NA,              U_NA
 ),
 
 [SYM] = LAYOUT(
-    KC_EXLM,           KC_AT,             KC_HASH,           KC_DLR,            KC_PERC,            KC_MUTE            KC_CIRC,           KC_AMPR,           KC_ASTR,           KC_GRAVE,          KC_TILD,
+    KC_EXLM,           KC_AT,             KC_HASH,           KC_DLR,            KC_PERC,            KC_MUTE,           KC_CIRC,           KC_AMPR,           KC_ASTR,           KC_GRAVE,          KC_TILD,
     KC_BSLS,           KC_PIPE,           KC_LPRN,           KC_RPRN,           KC_MINS,                               KC_EQL,            KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,
     KC_LT,             KC_GT,             KC_LCBR,           KC_RCBR,           KC_UNDS,                               KC_PLUS,           KC_LBRC,           KC_RBRC,           U_NA,              U_NA,
                                                              U_NA,              U_NA,                                  U_NA,              U_NA
 ),
 
 [NAV] = LAYOUT(
-    U_NA,              U_NA,              U_NA,              U_NA,              U_NA,               KC_MUTE            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            U_NA,
+    U_NA,              U_NA,              U_NA,              U_NA,              U_NA,               KC_MUTE,           KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            U_NA,
     KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,                                  KC_LEFT,           KC_DOWN,           KC_UP,             KC_RIGHT,          KC_BSPC,
     U_NA,              U_NA,              U_NA,              U_NA,              U_NA,                                  U_NA,              U_NA,              U_NA,              U_NA,              KC_DEL,
                                                              U_NA,              U_NA,                                  U_NA,              U_NA
 ),
 
 [FN] = LAYOUT(
-    U_NA,              U_NA,              U_NA,              U_NA,              U_NA,               KC_MUTE            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,
+    U_NA,              U_NA,              U_NA,              U_NA,              U_NA,               KC_MUTE,           U_NA,              U_NA,              U_NA,              U_NA,              U_NA,
     U_NA,              U_NA,              U_NA,              U_NA,              U_NA,                                  U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,
     U_NA,              U_NA,              U_NA,              U_NA,              U_NA,                                  U_NA,              U_NA,              U_NA,              U_NA,              U_NA,
                                                              U_NA,              U_NA,                                  U_NA,              U_NA
